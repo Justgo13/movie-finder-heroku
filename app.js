e595be1f43725b7cb466acb5820e71a58262bcb3
justgo13 <jgao2299@gmail.com>
2021-12-16 17:34:00 -0500

    static

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.use((req, res, next) => {
 
 app.use('/movie-search', movieRoutes);
 
+app.use(express.static(path.join(__dirname, "Client", "build")));
+
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, "Client", "build", "index.html"));
 });
