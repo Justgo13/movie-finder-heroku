b96065862bb33bd1d88df157bbdd949f96202a1d
justgo13 <jgao2299@gmail.com>
2021-12-16 16:12:52 -0500

    add gitingore

--- Client/src/App.js
+++ Client/src/App.js
@@ -13,15 +13,6 @@ function App() {
       <Routes>
         <Route path="movie-search" element={<MovieSearch />}/>
         <Route path="movie-search/results/:movieName" element={<SearchResult/>}/>
-
-        {/* <Route path="actor-search" element={<ActorSearch />}/>
-        <Route path="actor-search/results" element={<SearchResult/>}/>
-
-        <Route path="genre-search" element={<GenreSearch />}/>
-        <Route path="genre-search/results" element={<SearchResult/>}/>
-
-        <Route path="year-search" element={<YearSearch />}/>
-        <Route path="year-search/results" element={<SearchResult/>}/> */}
         <Route path="*" element={<Navigate replace to="/movie-search" />} />
       </Routes>
     </BrowserRouter>
